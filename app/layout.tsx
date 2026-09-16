a9ae1165cdee82b557644ee89bb3da873a793c42
falintino <falintino10@gmail.com>
2026-09-17 03:21:11 +0700

    feat: fokuskan storefront pada top up game

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,22 +12,20 @@ export const metadata: Metadata = {
   },
 
   description:
-    "Top Up Game, Jual Akun, Rekber, Rental Akun dan berbagai layanan gaming terpercaya.",
+    "Top up game dan berbagai layanan gaming terpercaya.",
 
   keywords: [
     "Top Up Free Fire",
     "Top Up ML",
     "Top Up PUBG",
     "Top Up Honor of Kings",
-    "Jual Akun",
     "Rekber",
-    "Rental Akun",
     "7 April Store",
   ],
 
   openGraph: {
     title: "7 April Store",
-    description: "Marketplace Gaming Indonesia",
+    description: "Layanan Produk Digital Game Indonesia",
     url: "https://store.falintino.com",
     siteName: "7 April Store",
     locale: "id_ID",
