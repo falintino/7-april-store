269a37b3e8f270b7c200b6a2f32c8f1469ec8ffd
falintino <falintino10@gmail.com>
2026-09-17 18:56:41 +0700

    Fix sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://7aprilstore.com/sitemap.xml",
+    sitemap: "https://store.falintino.com/sitemap.xml",
   };
 }
